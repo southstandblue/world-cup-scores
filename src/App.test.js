3a9729347b202944fdc1ca9a09082e11b7fe93c9
NickC <nick.fiona@virginmedia.com>
2023-07-11 10:44:51 +0100

    step 1 of the application - all tests should fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test the initial screen rendering of the application", () => {
+  test("App renders the Main Heading in the document", () => {
+    render(<App />);
+    const headingText = screen.getByText(/Live Football World Cup Scoreboard/i);
+    expect(headingText).toBeInTheDocument();
+  });
+  test("App renders the New Game button in the document", () => {
+    render(<App />);
+    const newGameButton = screen.getByRole("button");
+    expect(newGameButton).toBeInTheDocument();
+  });
+  test("App renders the Games in Progress Sub-Heading in the document", () => {
+    render(<App />);
+    const subHeadingText = screen.getByText(/Games in Progress/i);
+    expect(subHeadingText).toBeInTheDocument();
+  });
+  test("App renders the Game Summary Sub-Heading in the document", () => {
+    render(<App />);
+    const subHeadingText = screen.getByText(/Game Summary/i);
+    expect(subHeadingText).toBeInTheDocument();
+  });
 });
